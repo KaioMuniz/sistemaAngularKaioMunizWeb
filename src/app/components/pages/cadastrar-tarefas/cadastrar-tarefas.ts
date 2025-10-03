bc35c2fda916187831d64bd7a2fce4b7550b9754
Kaio Muniz <kkaioribeiro@gmail.com>
2025-10-03 20:22:09 -0300

    criei os componentes que vou usar até o momento: cadastrar tarefas, consultar tarefas, editar tarefas e o navbar no shared

---
+++ src/app/components/pages/cadastrar-tarefas/cadastrar-tarefas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-tarefas',
+  imports: [],
+  templateUrl: './cadastrar-tarefas.html',
+  styleUrl: './cadastrar-tarefas.css'
+})
+export class CadastrarTarefas {
+
+}
