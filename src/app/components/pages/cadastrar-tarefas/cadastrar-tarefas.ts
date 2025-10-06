c64e1f86dcefd71bab9079f2b7b859f33528a1db
Kaio Muniz <kkaioribeiro@gmail.com>
2025-10-06 12:13:19 -0300

    montei os validadores para os campos do forms, nome, data, prioridade e categoria id, e adicionei as mensagens de erro caso nao seja acertado o jeito de preencher

--- src/app/components/pages/cadastrar-tarefas/cadastrar-tarefas.ts
+++ src/app/components/pages/cadastrar-tarefas/cadastrar-tarefas.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-tarefas',
@@ -14,12 +14,17 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
 })
 export class CadastrarTarefas {
 
+
+
   formCadastro = new FormGroup({
-    nome: new FormControl(''),
-    data: new FormControl(''),
-    prioridade: new FormControl(''),
-    categoriaId: new FormControl(''),
+    nome: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    data: new FormControl('', [Validators.required]), 
+    prioridade: new FormControl('', [Validators.required]), 
+    categoriaId: new FormControl('', [Validators.required]), 
   });
+
+
+
 
 
   cadastrarTarefa() {
