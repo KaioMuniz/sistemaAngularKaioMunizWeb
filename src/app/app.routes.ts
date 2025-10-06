aeec67bc1c0f64e7ee6657db9db43d2d3b59c94e
Kaio Muniz <kkaioribeiro@gmail.com>
2025-10-06 11:54:53 -0300

    fiz o mapeamento das rotas de navegação para os componentes ja criados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { CadastrarTarefas } from './components/pages/cadastrar-tarefas/cadastrar-tarefas';
+import { ConsultarTarefas } from './components/pages/consultar-tarefas/consultar-tarefas';
+import { EditarTarefas } from './components/pages/editar-tarefas/editar-tarefas';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'pages/cadastrar-tarefas',
+        component: CadastrarTarefas
+    },
+    {
+        path: 'pages/consultar-tarefas',
+        component: ConsultarTarefas
+    },
+    {
+        path: 'pages/editar-tarefas',
+        component: EditarTarefas
+    }
+];
