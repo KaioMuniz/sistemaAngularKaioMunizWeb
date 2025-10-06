1de84343e4f579a4e8485867cf4c9f257c58650e
Kaio Muniz <kkaioribeiro@gmail.com>
2025-10-06 12:09:37 -0300

    fiz o formulario e esta chegando no console os dados

--- src/app/components/pages/cadastrar-tarefas/cadastrar-tarefas.ts
+++ src/app/components/pages/cadastrar-tarefas/cadastrar-tarefas.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-tarefas',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './cadastrar-tarefas.html',
   styleUrl: './cadastrar-tarefas.css'
 })
 export class CadastrarTarefas {
 
+  formCadastro = new FormGroup({
+    nome: new FormControl(''),
+    data: new FormControl(''),
+    prioridade: new FormControl(''),
+    categoriaId: new FormControl(''),
+  });
+
+
+  cadastrarTarefa() {
+    console.log(this.formCadastro.value);
+  }
 }
+
+
