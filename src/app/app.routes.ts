28e13262345d4d003f1607fe0fe8d8fc517da262
Kaio Muniz <kkaioribeiro@gmail.com>
2025-10-06 11:56:20 -0300

    adicionei mais um componenente 'dashboard' e fiz a rota de navegação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CadastrarTarefas } from './components/pages/cadastrar-tarefas/cadastrar-tarefas';
 import { ConsultarTarefas } from './components/pages/consultar-tarefas/consultar-tarefas';
 import { EditarTarefas } from './components/pages/editar-tarefas/editar-tarefas';
+import { Dashboard } from './components/pages/dashboard/dashboard';
 
 
 export const routes: Routes = [
@@ -16,5 +17,12 @@ export const routes: Routes = [
     {
         path: 'pages/editar-tarefas',
         component: EditarTarefas
+    },
+    {
+        path: 'pages/dashboard',
+        component: Dashboard
+    },
+    {
+        path: '', pathMatch: 'full', redirectTo: '/pages/dashboard'
     }
 ];
